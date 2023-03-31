de097b6930893c7fee2efc74cf47b024dc1f43bc
Josue Gallegos <gallegosjosue6@gmail.com>
2023-03-31 18:06:43 -0500

    complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me!';
+  } else if (distance > 400 &&  distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  } else if (city !== 'NYC') {
+    return 'No go.';
+  }
+
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else if (tip !== 'not as generous') {
+    return 'Bye.';
+  }
 }
